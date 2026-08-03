4bb494f3399ea231028b5ad6ca0e5a79878072b9
Zaid Jarrar <jarrarzaid3@gmail.com>
2026-08-03 14:58:48 +0200

    chore: regenerate windows plugin registrant after adding notification deps

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,12 +4,14 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
+  flutter_timezone
   passkeys_windows
   sqlite3_flutter_libs
   url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  flutter_local_notifications_windows
   jni
 )
 
